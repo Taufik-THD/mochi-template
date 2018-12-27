e896ac24aade97057245b37212217a3e499a9c59
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-12-28 05:36:08 +0700

    Update boilerplate

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,28 @@
 import m from 'mithril'
 import marked from 'marked'
-import Hello from '../markdown/hello.md'
+import Hello from '../documents/hello.md'
 
 import 'mochi-bootstrap'
 
-import './style.scss'
+import '../styles/main.scss'
 
 export default class Home {
   view() {
     return (
       <main>
-<p>
-  <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-    Link with href
-  </a>
-  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    Button with data-target
-  </button>
-</p>
-<div class="collapse" id="collapseExample">
-  <div class="card card-body">
-    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-  </div>
-</div>
+        <p>
+          <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Link with href
+          </a>
+          <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            Button with data-target
+          </button>
+        </p>
+        <div class="collapse" id="collapseExample">
+          <div class="card card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+        </div>
 
         <h1 class="green">Page Home</h1>
         {m.trust(marked(Hello))}
