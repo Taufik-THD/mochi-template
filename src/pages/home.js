081f21ef675fe2f39dfca3400c2d630bce2cd0e2
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-12-27 12:22:31 +0700

    Add markdown support

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,13 @@
 import m from 'mithril'
 
+import Hello from '../markdown/hello.md'
+
 export default class Home {
   view() {
     return (
       <main>
         <h1>Page Home</h1>
+        <p>{Hello}</p>
         <a href="#!/about">Page About</a>
       </main>
     )
