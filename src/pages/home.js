e142f7504cf729acc4d78340b703ec4b5b2eb3be
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-12-27 19:52:38 +0700

    [Done] implement SASS/SCSS

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,14 @@
 import m from 'mithril'
 import convert from 'html-to-jsx'
-
 import Hello from '../markdown/hello.md'
+
+import './style.scss'
 
 export default class Home {
   view() {
     return (
       <main>
-        <h1>Page Home</h1>
+        <h1 class="green">Page Home</h1>
         {convert(Hello)}
         <a href="#!/about">Page About</a>
       </main>
